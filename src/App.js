2dd9c80fb08db3475654cbf854b2cbaf0808008d
exp-nazeem <muhammed.nazeem@experionglobal.com>
2022-08-09 10:01:55 +0530

    Products and Cart

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import Header from './components/header';
+import CartPage from './pages/cart';
+import ProductPage from './pages/product';
 import LoginPage from './pages/login';
 import HomePage from './pages/home';
 
@@ -13,6 +15,14 @@ function App() {
 
       <div className="page-container">
         <Switch>
+          <Route exact path="/cart">
+            <CartPage />
+          </Route>
+
+          <Route exact path="/product/:productId">
+            <ProductPage />
+          </Route>
+
           <Route exact path="/login">
             <LoginPage />
           </Route>
